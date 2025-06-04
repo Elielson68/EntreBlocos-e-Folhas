27d878546b445e5742d40e0f891a05d9639f6e46
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-06-04 19:54:57 -0300

    feat: add tela do vale original e colisão no mapa

--- Assets/MobController.cs
+++ Assets/MobController.cs
@@ -30,7 +30,6 @@ public class MobController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("entrou aqui");
         if (collision.collider.tag == "Player")
         {
             Debug.Log("Colidiu com o player!");
