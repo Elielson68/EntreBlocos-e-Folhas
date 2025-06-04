17a8413cb53d44283b11ee39088878e2dd250d48
Elielson Barbosa <elielsonbr.com@gmail.com>
2025-06-04 16:39:02 -0300

    feat: add mobs, add dotween e movimentação do mob

---
+++ Assets/MobController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MobController : MonoBehaviour
+{
+    public List<Transform> PositionsToMove;
+    public int PositionToMove;
+    private int _lastPosition;
+    void Start()
+    {
+        MoveMob();
+    }
+
+    private void MoveMob()
+    {
+        _lastPosition = PositionToMove;
+
+        while (PositionToMove == _lastPosition)
+        {
+            PositionToMove = Random.Range(0, PositionsToMove.Count);
+        }
+
+        float velocity = Random.Range(0.3f, 3);
+
+        Ease curveMove = Random.Range(0, 2) == 1 ? Ease.OutExpo : Ease.Linear;
+
+        transform.DOMoveX(PositionsToMove[PositionToMove].position.x, velocity).SetEase(curveMove).OnComplete(MoveMob);
+    }
+}
